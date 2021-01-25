50b2d55c6b3364d2c2bb1f12425153860d10d40a
pk1510 <63361815+pk1510@users.noreply.github.com>
2021-01-25 18:53:08 +0530

    Update demo_webcam.py

--- demo_webcam.py
+++ demo_webcam.py
@@ -5,7 +5,7 @@ import vlc
 import time
 from yolo import YOLO
 from operator import itemgetter
-ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser()                                                                              #parsing command line arguements
 ap.add_argument('-n', '--network', default="normal", help='Network Type: normal / tiny / prn / v4-tiny')
 ap.add_argument('-d', '--device', default=0, help='Device to use')
 ap.add_argument('-s', '--size', default=416, help='Size for yolo')
@@ -28,15 +28,8 @@ else:
 yolo.size = int(args.size)
 yolo.confidence = float(args.confidence)
 
-
-img = cv2.imread("timeStone.jpg")
-lower_bgr = np.uint8([[[30, 30, 30]]])
-upper_bgr = np.uint8([[[255, 255, 255]]])
-mask = cv2.inRange(img, lower_bgr, upper_bgr)
-img = cv2.bitwise_and(img, img, mask=mask)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-vdo = ("test_vdo.mp4", "Faded.mp3")                              ##add ur vdo list here and access via the index
+#add ur vdo list here and access via the index
+vdo = ("test_vdo.mp4", "Faded.mp3")                              
 # create video capture object
 data = cv2.VideoCapture(vdo[0])
 
@@ -49,14 +42,20 @@ seconds = int(frames / fps)
 milliseconds = seconds*1000
 data.release()
 
+#initialize GP parameters
 a = 500
 r = 1.0001
 n = 0
-direction = "rest"                                                 #direction of rotation. Use this variable with vlc library
+
+#direction of rotation. Use this variable with vlc library
+direction = "rest"
+
+#create an instance of MediaPlayer class to play the video in VLC
 media_player = vlc.MediaPlayer()
 media = vlc.Media(vdo[0])
 media_player.set_media(media)
 
+#Variable used to keep track of the start of the gesture
 first = False
 
 print("starting webcam...")
@@ -71,18 +70,8 @@ else:
 while rval:
     width, height, inference_time, results = yolo.inference(flip)
     if len(results)!=0:
+        #find the most confident detection of the model
         res = max(results, key=itemgetter(2))
-    # for detection in results:
-    #     id, name, confidence, x, y, w, h = detection
-    #     cx = x + (w / 2)
-    #     cy = y + (h / 2)
-    #
-    #     # draw a bounding box rectangle and label on the image
-    #     color = (0, 255, 255)
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-    #     text = "%s (%s)" % (name, round(confidence, 2))
-    #     cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-    #                 0.5, color, 2)
         id, name, confidence, x, y, w, h = res
         img = cv2.resize(img, (w,h))
         gray = cv2.resize(gray, (w, h))
@@ -98,7 +87,8 @@ while rval:
         cv2.putText(flip, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, color, 2)
         cv2.circle(flip, (cx, cy), 5, (0,0,0), -1)
-        #flip[y:y+h, x:x+w, :] = np.where(thresh != 0, img, flip[y:y+h, x:x+w, :])
+        
+        #Feel free to tune these hyperparameters based on the comfort area of your hand
         if cx >=420 and cx < 510 and w>=120 and w<=220:
             direction = "rest"
         elif cx>=505 and cx<=600 and w>=190 and w<=290:
@@ -107,10 +97,10 @@ while rval:
             direction = "anticlockwise"
         else:
             direction = "rest"
-        print(direction, cx, cy, w, h)
+        #print(direction, cx, cy, w, h)
     else:
         direction="rest"
-        #print(cx, cy, w, h)
+        
     media_player.play()
     prev = direction
     if direction == "anticlockwise":
@@ -121,10 +111,12 @@ while rval:
             if not first:
                 first = True
                 n=0
-                #r = 1 - a/curr_time
+              
             n+=1
+            #Use the Geometric progression series to travel through time
             media_player.set_time(curr_time - int(a*pow(r, n)))
             media_player.play()
+            #Execution time is far too less than the actual video playing time. So give some time to put everything in the same workflow
             time.sleep(0.1)
     if direction == "clockwise":
         curr_time = media_player.get_time()
@@ -134,7 +126,7 @@ while rval:
             if not first:
                 n = 0
                 first = True
-                #r = 1 - curr_time/milliseconds if curr_time!=0 else 1 - a/milliseconds
+                
             n+=1
             media_player.set_time(curr_time + int(a*pow(r, n)))
             time.sleep(0.1)
